7e02e434eedda9e5dbbcc35f218fa464644a6d98
Max Gonzih <gonzih@gmail.com>
2015-11-07 20:03:41 +0100

    Start emacs daemon for the convinience

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-# cd /src
+cd /src
+
+emacs --daemon
 
 tmux -2 -u new -s main -d
 
@@ -6,6 +8,6 @@ session=main
 window=${session}:1
 pane=${window}.0
 
-tmux -2 -u send-key -t $pane "emacs -nw ." Enter
+tmux -2 -u send-key -t $pane "emacsclient -nw ." Enter
 
 exec tmux -2 -u attach -t $session
